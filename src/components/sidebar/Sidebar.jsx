fbe69e52a6a1ef1195464da17427d05d34b5f3eb
yasminaisha <jasyahmed1@yahoo.com>
2022-05-12 22:17:47 +0300

    link updates to sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,10 +10,12 @@ export default function Sidebar() {
             <div className="siderbarMenu">
                  <h3 className="sidebarTitle">Dashboard</h3>
                  <ul className="sidebarList">
+                    <Link to="/" className="link">
                      <li className="sidebarListItem active">
                          <LineStyle className="sidebarIcon"/>
                          Home
                      </li>
+                    </Link>
                      <li className="sidebarListItem">
                          <Timeline className="sidebarIcon"/>
                          Analytics
